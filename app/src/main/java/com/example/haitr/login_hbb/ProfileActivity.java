c2f8e6939f7d923fb512cc60ad67f341c0b2fea9
Haitrancntt <haitran2402@gmail.com>
2016-09-06 08:54:40 +0700

    profile activity

---
+++ app/src/main/java/com/example/haitr/login_hbb/ProfileActivity.java
@@ -0,0 +1,49 @@
+package com.example.haitr.login_hbb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.login.widget.ProfilePictureView;
+
+import Class.Account_Facebook;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ProfileActivity extends AppCompatActivity {
+    private TextView txtuserName, txtuserEmail, txtuserGender, txtuserBirthday;
+    private ProfilePictureView profilePictureView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        txtuserEmail = (TextView) findViewById(R.id.txtEmail);
+        txtuserName = (TextView) findViewById(R.id.txtName);
+        txtuserGender = (TextView) findViewById(R.id.txtGender);
+        txtuserBirthday = (TextView) findViewById(R.id.txtBirthday);
+        profilePictureView = (ProfilePictureView) findViewById(R.id.profilepicture);
+        Account_Facebook account_facebook;
+        Intent intent = getIntent();
+        account_facebook = (Account_Facebook) intent.getSerializableExtra("profile");
+        txtuserEmail.setText(account_facebook.getEmail());
+        txtuserName.setText(account_facebook.getName());
+        txtuserGender.setText(account_facebook.getGender());
+        txtuserBirthday.setText(account_facebook.getBirthday());
+        profilePictureView.setPresetSize(ProfilePictureView.NORMAL);
+        profilePictureView.setProfileId(account_facebook.getId());
+        //txtuserName.setText(account_facebook.ToString());
+
+    }
+
+   /* public void setProfile(JSONObject jsonObject) {
+        try {
+            txtuserName.setText(jsonObject.getString("name"));
+            txtuserEmail.setText(jsonObject.getString("email"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }*/
+}
